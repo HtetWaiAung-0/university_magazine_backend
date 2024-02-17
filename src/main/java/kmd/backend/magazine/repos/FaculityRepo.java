e0629f170523382b96e273ac512325d6489c96d3
Htet Wai Aung <htetwaiaung1236@gmail.com>
2024-02-18 00:38:53 +0630

    202402180038hwa

--- src/main/java/kmd/backend/magazine/repos/FaculityRepo.java
+++ src/main/java/kmd/backend/magazine/repos/FaculityRepo.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import kmd.backend.magazine.models.Faculty;
 
-public interface FaculitiesRepo extends JpaRepository<Faculty, Integer> {
+public interface FaculityRepo extends JpaRepository<Faculty, Integer> {
     
 }
--- src/main/java/kmd/backend/magazine/services/FaculityService.java
+++ src/main/java/kmd/backend/magazine/services/FaculityService.java
@@ -3,10 +3,10 @@ package kmd.backend.magazine.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import kmd.backend.magazine.repos.FaculitiesRepo;
+import kmd.backend.magazine.repos.FaculityRepo;
 
 @Service
-public class FaculitieService {
+public class FaculityService {
     @Autowired
-    private FaculitiesRepo faculitiesRepo;
+    private FaculityRepo faculityRepo;
 }
