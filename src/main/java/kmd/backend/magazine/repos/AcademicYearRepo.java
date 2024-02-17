91c669d218a0cf1d92103e32bcaeb3b021606c18
Htet Wai Aung <htetwaiaung1236@gmail.com>
2024-02-18 00:57:15 +0630

    202402180057hwa

--- src/main/java/kmd/backend/magazine/repos/AcademicYearRepo.java
+++ src/main/java/kmd/backend/magazine/repos/AcademicYearRepo.java
@@ -2,7 +2,9 @@ package kmd.backend.magazine.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import kmd.backend.magazine.models.AcademicYear;
+import java.util.List;
+
 
 public interface AcademicYearRepo extends JpaRepository<AcademicYear, Integer> {
-    
+    List<AcademicYear> findByName(String name);
 }
--- src/main/java/kmd/backend/magazine/services/AcademicYearService.java
+++ src/main/java/kmd/backend/magazine/services/AcademicYearService.java
@@ -1,8 +1,42 @@
 package kmd.backend.magazine.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kmd.backend.magazine.exceptions.EntityAlreadyExistException;
+import kmd.backend.magazine.exceptions.EntityNotFoundException;
+import kmd.backend.magazine.models.AcademicYear;
+import kmd.backend.magazine.repos.AcademicYearRepo;
+import java.util.List;
 @Service
 public class AcademicYearService {
-    
+    @Autowired
+    private AcademicYearRepo academicYearRepo;
+
+    public AcademicYear getAcademicYear(int id) {
+        return academicYearRepo.findById(id).orElseThrow(() -> new EntityNotFoundException("AcademicYear ID : " + id));
+    }
+
+    public List<AcademicYear> getAllAcademicYears() {
+        return academicYearRepo.findAll();
+    }
+
+    public AcademicYear saveAcademicYear(AcademicYear academicYear) {
+        List<AcademicYear> existingAcademicYears = academicYearRepo.findByName(academicYear.getName());
+        if(existingAcademicYears.isEmpty()) {
+            return academicYearRepo.save(academicYear);
+        }else{
+            throw new EntityAlreadyExistException("AcademicYear Name : " + academicYear.getName());
+        }
+        
+    }
+
+    public void deleteAcademicYear(int academicYearId) {
+        AcademicYear academicYear = academicYearRepo.findById(academicYearId).get();
+        if (academicYear!= null) {
+            academicYearRepo.deleteById(academicYearId);
+        } else {
+            throw new EntityNotFoundException("AcademicYear ID : " + academicYearId);
+        }
+    }
 }
