19edf7587d59b44a2bdadbb1b7cc548cf541fcd4
Htet Wai Aung <htetwaiaung1236@gmail.com>
2024-02-28 22:44:26 +0630

    202402282244hwa

--- src/main/java/kmd/backend/magazine/controllers/UserController.java
+++ src/main/java/kmd/backend/magazine/controllers/UserController.java
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import kmd.backend.magazine.models.Article;
 import kmd.backend.magazine.models.User;
 import kmd.backend.magazine.services.UserService;
 
@@ -23,6 +25,8 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @GetMapping
     public List<User> getAllUsers() {
@@ -40,8 +44,9 @@ public class UserController {
         return ResponseEntity.ok().body("User deleted");
     }
 
-    @PostMapping("/uploadProfile")
-    public ResponseEntity<?> profilePhotoUpload(@RequestParam("user")User user, @RequestParam("profilePhoto")MultipartFile profilePhoto)throws IOException{
+    @PostMapping("/add")
+    public ResponseEntity<?> profilePhotoUpload(@RequestParam("user")String userStr, @RequestParam("profilePhoto")MultipartFile profilePhoto)throws IOException{
+         User user = objectMapper.readValue(userStr, User.class);
          userService.savedUser(user,profilePhoto);
          return ResponseEntity.ok().body("User Added!!"); 
     }
--- src/main/java/kmd/backend/magazine/services/AcademicYearService.java
+++ src/main/java/kmd/backend/magazine/services/AcademicYearService.java
@@ -2,7 +2,6 @@ package kmd.backend.magazine.services;
 
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
