ca6f35f39e4624802b208472082e0dfd6e60ead9
Htet Wai Aung <htetwaiaung1236@gmail.com>
2024-03-10 10:12:38 +0630

    20241003

--- src/main/java/kmd/backend/magazine/config/ResourceHandler.java
+++ src/main/java/kmd/backend/magazine/config/ResourceHandler.java
@@ -1,19 +1,17 @@
 package kmd.backend.magazine.config;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ResourceHandler implements WebMvcConfigurer{
     
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry){
-        Path fileUploadPath =Paths.get("./assets/");
-        String fileUploadDirectory = fileUploadPath.toFile().getAbsolutePath();
-
-        registry.addResourceHandler("/assets/**").addResourceLocations("file:/"+ fileUploadDirectory + "/");
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow requests from all paths
+                .allowedOrigins("*") // Allow requests from all origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
 }
