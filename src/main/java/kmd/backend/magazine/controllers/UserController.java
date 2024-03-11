747a1d0bb4f0fd456920e4b7ac251781951d8768
ZarZarSoe1010 <73434372+ZarZarSoe1010@users.noreply.github.com>
2024-03-12 01:22:59 +0630

    20240312ZZS

--- src/main/java/kmd/backend/magazine/controllers/UserController.java
+++ src/main/java/kmd/backend/magazine/controllers/UserController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 
---
+++ src/main/java/kmd/backend/magazine/dtos/FacultyResponseDto.java
@@ -0,0 +1,12 @@
+package kmd.backend.magazine.dtos;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FacultyResponseDto {
+    private int id;
+    private String name;
+    
+} 
