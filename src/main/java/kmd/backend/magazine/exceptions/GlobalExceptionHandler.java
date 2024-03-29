75c3979fbe6ed0032014c635c6b6b457028711be
Pkar <pkar@Pkars-MacBook-Pro.local>
2024-03-29 23:57:46 +0700

    202403290000

--- src/main/java/kmd/backend/magazine/exceptions/GlobalExceptionHandler.java
+++ src/main/java/kmd/backend/magazine/exceptions/GlobalExceptionHandler.java
@@ -13,10 +13,10 @@ import org.springframework.web.servlet.resource.NoResourceFoundException;
 @ControllerAdvice
 public class GlobalExceptionHandler  {
 
-    // @ExceptionHandler(BadRequestException.class)
-    // public ResponseEntity<String> handleBadRequestException(BadRequestException e) {
-    //     return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-    // }
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> handleBadRequestException(BadRequestException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException e) {
@@ -33,20 +33,20 @@ public class GlobalExceptionHandler  {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.METHOD_NOT_ALLOWED);
     }
 
-    // @ExceptionHandler(IllegalArgumentException.class)
-    // public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
-    //     return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-    // }
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(NoResourceFoundException.class)
     public ResponseEntity<String> handleNoResourceFoundException(NoResourceFoundException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
-    // @ExceptionHandler(Exception.class)
-    // public ResponseEntity<String> handleException(Exception e) {
-    //     return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-    // }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<String> handleAuthenticationException(AuthenticationException e) {
